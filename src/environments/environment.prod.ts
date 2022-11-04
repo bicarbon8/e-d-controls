7d40e40b7ef2f2231f2781e8aee5c0f4e91730de
Jason Holt Smith <jason.smith@verizonconnect.com>
2022-11-04 13:16:02 +0000

    asset path fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseHref: '/e-d-controls',
-  defaultBindingsFile: '/assets/bindings/Custom.3.0.binds'
+  defaultBindingsFile: 'https://bicarbon8.github.io/e-d-controls/assets/bindings/Custom.3.0.binds'
 };
